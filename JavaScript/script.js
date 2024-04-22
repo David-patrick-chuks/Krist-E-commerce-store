4b94bc95e50602e02382d98536895d268ca1173c
David-patrick-chuks <pd3072894@gmail.com>
2024-04-22 17:06:12 +0100

    krist

---
+++ JavaScript/script.js
@@ -0,0 +1,10 @@
+var name = "Solomon"; //String
+var age = 20; //integer
+var height = 23.9  //float or double
+var isShort = true; //boolean
+var car = null;   //null
+var home; //undefined
+
+
+
+console.log(name+age);
